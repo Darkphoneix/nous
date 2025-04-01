b35a6872a9f5324dd985237a1ee1089be2a27d8e
Darkphoneix <efecamzeybek@gmail.com>
2025-04-01 22:37:20 +0300

    ilk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,10 @@ android {
     namespace = "com.example.nous"
     compileSdk = 35
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.nous"
         minSdk = 24
